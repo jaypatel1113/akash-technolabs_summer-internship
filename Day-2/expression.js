988f935b65edeb8b5dc80ec37d69c76fbc19b196
Jay Patel <jay64441860@gmail.com>
2022-06-23 13:38:04 +0530

    NodeBeg & HTML Overview

---
+++ Day-2/expression.js
@@ -0,0 +1,10 @@
+var a=10;
+var b=20;
+
+console.log("a: "+ a + " - b: " + b);
+console.log(`a: ${a} - b: ${b}`);   //ES6 syntax
+
+var c = a+b;
+
+console.log("sum = "+ c);
+console.log(`sum = ${c}`);
