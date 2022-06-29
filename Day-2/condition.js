034e64518e272db64a070a8e88b4510945721d11
Jay Patel <jay64441860@gmail.com>
2022-06-29 20:46:05 +0530

    added express routing app

--- Day-2/condition.js
+++ Day-2/condition.js
@@ -3,7 +3,7 @@ var b=20;
 var c = a+b;
 
 if(c==30) {
-    console.log("c is 30");
+    console.log("\nc is 30\n");
 } else {
-    console.log("c is not 30");
+    console.log("\nc is not 30\n");
 }
--- Day-2/expression.js
+++ Day-2/expression.js
@@ -1,10 +1,11 @@
 var a=10;
 var b=20;
 
-console.log("a: "+ a + " - b: " + b);
-console.log(`a: ${a} - b: ${b}`);   //ES6 syntax
+console.log("\na: "+ a + " - b: " + b);
+console.log(`a: ${a} - b: ${b}\n`);   //ES6 syntax
 
 var c = a+b;
 
 console.log("sum = "+ c);
-console.log(`sum = ${c}`);
+console.log(`sum = ${c}\n`);
+// same goes with - * / ** etc etc 
